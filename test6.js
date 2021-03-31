86ba79aaa3383195ca460bb57b3bb085c83d0120
guyzion <guyzion7@gmail.com>
2021-03-31 15:33:36 +0300

    Update and rename test5.js to test6.js

--- test6.js
+++ test6.js
@@ -4,8 +4,8 @@
 		console.log("hi im guy zion");
 
 		var sourceControlId = 96197229, /** THE ID OF THE SENDER CONTROL */
-
-		var myControl = loader.getEngine().getDocument().getElementById(sourceControlId);
+		    
+		myControl = loader.getEngine().getDocument().getElementById(sourceControlId);
 
 		myControl.on('value-change', function(){
 
