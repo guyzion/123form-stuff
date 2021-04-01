4bf382a21e94643b33c126ca7f0dc212c30ce571
guyzion <guyzion7@gmail.com>
2021-04-01 18:26:55 +0300

    Update and rename test27.js to test28.js

--- test28.js
+++ test28.js
@@ -4,10 +4,12 @@
 		console.log("hi im guy zion");
 		
 		try {
-			var sumControlId = 96233099, /** THE ID OF THE SENDER CONTROL */
+			var sumControlId = 96233099, 
 			sumControl = loader.getEngine().getDocument().getElementById(sumControlId);
+			var finalEmailControlId = 96233355,
+			finalEmailControl = loader.getEngine().getDocument().getElementById(finalEmailControlId);
 
-			sumControl.on('value-change', function(){
+			finalEmailControl.on('value-change', function(){
 				var sum = sumControl.getValue().value
 				localStorage.setItem("sum", sum);
 				console.log(sum);
